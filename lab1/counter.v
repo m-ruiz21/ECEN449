8cb2f70755ba755bf1c32d983907e128da51e53b
Mateo Ruiz <m.ruiz_21@zach-333-em3-32.engr.tamu.edu>
2023-02-02 12:48:38 -0600

    something?

--- lab1/counter.v
+++ lab1/counter.v
@@ -1,18 +1,18 @@
 `timescale 1ns / 1ps
 
-module switch(
+module counter(
     input BUTTONS[1:0],
     input clk, 
     input rst,
-    output reg [3:0] LEDS,
+    output reg [3:0] LEDS
     );
     
     always @(posedge clk or posedge rst) begin 
         if (rst)
-            LEDS <= 4`b0;
-        else if (BUTTONS == 2`b01)
-            LEDS <= LEDS + 4`b1
-        else if (BUTTONS == 2`b10)
-            LEDS <= LEDS - 4`b1
+            LEDS <= 2'b0;
+        else if (BUTTONS == 2'b01)
+            LEDS <= LEDS + 4'b1;
+        else if (BUTTONS == 2'b10)
+            LEDS <= LEDS - 4'b1;
     end
 endmodule
